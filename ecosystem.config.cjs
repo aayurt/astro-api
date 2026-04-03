e99f0aba182e055045bc995217fe67926acb9e3c
Aayurt <aayurtshrestha@gmail.com>
2026-04-04 00:26:29 +0545

    Changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,8 +10,8 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
       },
-      env_production: {
-        NODE_ENV: 'production',
+      env_development: {
+        NODE_ENV: 'development',
       },
     },
   ],
