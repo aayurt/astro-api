bc3f8366099c3e6aa4bd372964e023974ba2077c
Aayurt <aayurtshrestha@gmail.com>
2026-04-03 23:14:40 +0545

    Changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 import { toNodeHandler } from 'better-auth/node';
 import cors from 'cors';
 import 'dotenv/config';
