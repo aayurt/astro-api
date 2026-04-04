f2ab7ea2545230e1816b6e3a8641065c854ee4df
Aayurt <aayurtshrestha@gmail.com>
2026-04-04 17:49:42 +0545

    Changs

--- src/trustedDomains.js
+++ src/trustedDomains.js
@@ -1,4 +1,4 @@
-export const trustedOrigins = [
+const origins = [
   process.env.FRONTEND_URL,
   'http://localhost:5173',
   'https://astro-app.vercel.app',
@@ -6,3 +6,14 @@ export const trustedOrigins = [
   'http://localhost:3006',
   'http://localhost:3001',
 ];
+
+export const trustedOrigins = Array.from(
+  new Set(
+    origins
+      .filter(Boolean)
+      .flatMap((origin) => [
+        origin.replace(/\/$/, ''),
+        `${origin.replace(/\/$/, '')}/`,
+      ]),
+  ),
+);
