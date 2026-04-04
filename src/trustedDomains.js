1b46e84ed3bbdb8e015eda2edd76a7429842e999
Aayurt <aayurtshrestha@gmail.com>
2026-04-04 17:40:52 +0545

    Changes

--- src/trustedDomains.js
+++ src/trustedDomains.js
@@ -1,4 +1,8 @@
 export const trustedOrigins = [
   process.env.FRONTEND_URL,
   'http://localhost:5173',
+  'https://astro-app.vercel.app',
+  'https://ratosuryaonline.com/astro',
+  'http://localhost:3006',
+  'http://localhost:3001',
 ];
